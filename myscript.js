5e70c19c6578247c769d43c83cb357366d21d4b5
nguyelon002 <nguyelon002@edmonds15.org>
2017-05-19 12:53:07 -0700

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,20 @@
+  var myHeading = document.querySelector('h1');
+  myHeading.textContent = 'Hello world!';
+
+var myButton = document.querySelector('button');
+          var myHeading = document.querySelector('h1');
+          function setUserName() {
+            var myName = prompt('Please enter your name.');
+            localStorage.setItem('name', myName);
+            myHeading.textContent = 'Brawhalla is cool, ' + myName;
+            if(!localStorage.getItem('name')) {
+              setUserName();
+            } else {
+              var storedName = localStorage.getItem('name');
+              myHeading.textContent = 'Brawhalla is cool, ' +                       storedName;
+            }
+          }
+          
+          myButton.onclick = function() {
+          setUserName();
+          };
